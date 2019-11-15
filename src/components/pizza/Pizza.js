955bf796897a59b9bfa344b78b7e23ab9e6000c5
t-kowalewski <wkowalewski@outlook.com>
2019-11-15 14:04:13 +0100

    add ingredients toggle logic

--- src/components/pizza/Pizza.js
+++ src/components/pizza/Pizza.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import './pizza.scss';
 
 export default class Pizza extends Component {
+  // Our state - show <p> or not
+  state = {
+    showIngredients: false,
+    arrowDown: true,
+    arrowUp: false
+  }
+
+  // Toggle function
+  handleIngredients = () => {
+    this.setState({
+      showIngredients: !this.state.showIngredients,
+      arrowDown: !this.state.arrowDown,
+      arrowUp: !this.state.arrowUp
+    })
+  }
+
   render() {
     console.log(this.props);
     const { id, img, ingredients, pizza, pronunciation } = this.props.pizza;
@@ -17,8 +33,13 @@ export default class Pizza extends Component {
         <div className="pizza-info">
           <h2>{pizza}</h2>
           <h4>{pronunciation}</h4>
-          <h4>Ingredients <span><i className="fas fa-chevron-down fa-sm"></i></span></h4>
-          <p>{ingredients}</p>
+
+          {/* JSX condition for arrow icon inside <span> */}
+          <h4>Ingredients <span onClick={this.handleIngredients}>{this.state.arrowDown && <i className="fas fa-chevron-down fa-sm"></i>}{this.state.arrowUp && <i className="fas fa-chevron-up fa-sm"></i>}</span></h4>
+
+          {/* JSX condition for <p> */}
+          {this.state.showIngredients && <p>{ingredients}</p>}
+
           <span className="close-btn"><i className="far fa-times-circle"></i></span>
         </div>
       </article>
